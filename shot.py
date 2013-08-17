539ad00b2c1ba69cb9c072d10f5547f65b0bdc52
Yan Jiang <yjiang@eee168.com>
2013-08-17 23:09:13 +0800

    time refresh fixed

--- shot.py
+++ shot.py
@@ -7,18 +7,19 @@ from pygame.locals import *
 resolution = (640, 480)
 devicePath = "/dev/video0"
 IMG = './shot.jpg'
-now = time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
 
 pygame.init()
 def create_date_img(): #{{{
+    now = time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
     pygame.font.init()
-    font = pygame.font.SysFont("ubuntu", 20)
+    font = pygame.font.SysFont("wenquanyizenhei", 20)
     image = font.render(now, True, (255, 255, 255))
     #pygame.image.save(image, IMG)
     return image
 #}}}
 def scp(source_path, obj_path): #{{{
-    os.system('scp ./shot.jpg www-data@582033.vicp.net:~/cake/other/ > /dev/null &')
+    os.system('scp %s www-data@42.96.185.104:~/cake/other/ > /dev/null' % (IMG))
+    print ''
 #}}}
 def output(): #{{{
     pygame.camera.init()
@@ -26,14 +27,24 @@ def output(): #{{{
     cam = pygame.camera.Camera(devicePath, (resolution[0],resolution[1]))
     cam.start()
     cam_image = cam.get_image()
-    cam_image.blit(image, (460, 450))
+    cam_image.blit(image, (440, 450))
     pygame.image.save(cam_image, IMG)
     cam.stop()
     scp(IMG, '/var/www/cake/other/')
 #}}}
 def sleep(): #{{{
+    counter = 0
     while True:
         output()
-        time.sleep(10)
+        counter += 1
+        print counter
+        time.sleep(1)
 #}}}
+def print_all_fonts():#{{{
+    pygame.font.init()
+    print pygame.font.get_fonts()
+#}}}
+#print_all_fonts()
 sleep()
+
+
